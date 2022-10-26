680eed7efe5fd3a27ba502275493a3da503ff2d8
jsevilla <jsevilla@106-19.info2>
2022-10-26 14:33:09 +0200

    Ejercicios While

--- Ejercicios While/src/ejercicioswhile/Ejercicio6.java
+++ Ejercicios While/src/ejercicioswhile/Ejercicio6.java
@@ -12,10 +12,7 @@ public class Ejercicio6 {
 		System.out.println("A continuación se le pedirá la edad de los alumnos, para salir del programa use un número negativo");
 		System.out.print("Por favor, introduzca la edad: ");
 		edad = sc.nextInt();
-		if(edad==-1) {
-			System.out.println("Se ha cerrado el programa");
-		}
-		else {
+		if(edad!=-1) {
 		while(edad>=1) {
 			nalumnos++;
 			edadtotal = edadtotal + edad;
@@ -27,6 +24,9 @@ public class Ejercicio6 {
 		}
 		System.out.println("La suma total de las edades es de: "+edadtotal+"\nLa media de las edades es: "+(double)(edadtotal/nalumnos)+
 				"\nEl número de alumnos es: "+nalumnos+"\nHay un total de "+mayoredad+" alumnos mayores de edad");
+		}
+		else {
+			System.out.println("Se ha cerrado el programa");
 		}
 		sc.close();
 
